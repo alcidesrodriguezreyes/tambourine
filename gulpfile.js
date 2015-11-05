d47ac035b82ca5635bcbbe1fa4c28431fdcf5954
Alcides <alcides.rodriguez@prodigious.com>
2015-11-05 00:01:39 -0500

    add sass watcher

--- gulpfile.js
+++ gulpfile.js
@@ -7,29 +7,27 @@ var connect = require('gulp-connect');
 
 
 gulp.task('sass', function(){
-    gulp.src('./sass/**/*.scss')
+    gulp.src('./stylesheets/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
-});
-
-gulp.task('sass:watch', function(){
-   gulp.watch('./sass/**/*.scss', ['sass']) 
+        .pipe(gulp.dest('./app/css'))
+        .pipe(connect.reload());
 });
 
 gulp.task('connect', function() {
-  connect.server({
-    root: 'app',
-    livereload: true
-  });
+    connect.server({
+        root: 'app',
+        livereload: true
+    });
 });
  
 gulp.task('html', function () {
-  gulp.src('./app/*.html')
-    .pipe(connect.reload());
+    gulp.src('./app/*.html')
+        .pipe(connect.reload());
 });
  
 gulp.task('watch', function () {
-  gulp.watch(['./app/*.html'], ['html']);
+    gulp.watch(['./app/*.html'], ['html']);
+    gulp.watch('./stylesheets/**/*.scss', ['sass']);
 });
  
 gulp.task('default', ['connect', 'watch']);
