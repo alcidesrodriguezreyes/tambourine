6e4ed2e9b0fb8355207dd04499755302ae378035
Alcides Rodriguez <alcides.rodriguez@prodigious.com>
2015-11-06 20:35:22 -0500

    mobile version

--- app/js/app/app.js
+++ app/js/app/app.js
@@ -4,4 +4,27 @@
         autoplay: true,
         autoplaySpeed: 8000
     });
+    
+    $('[data-toggle="slider-mobile"]').slick({
+        mobileFirst: true,
+        variableWidth: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: false,
+        responsive: [
+        {
+            breakpoint: 640
+        },{
+            breakpoint: 700,
+            settings: 'unslick'
+        },{
+            breakpoint: 1024,
+            settings: 'unslick'
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+        ]
+    });
 })(jQuery);
